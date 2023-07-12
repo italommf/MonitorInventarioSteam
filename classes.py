fbba3e59784b513b87c7ab5f76c30738c8d50426
italommf <immf.estudo@gmail.com>
2023-07-12 17:41:07 -0300

    Correção do botão atualizar inventário

--- classes.py
+++ classes.py
@@ -46,7 +46,7 @@ class Inventario:
 
         self.nome = nome
         self.custoporitem = custoporitem
-        self.custoporitem = self.custoporitem.replace(',','.')
+        self.custoporitem = self.custoporitem.replace(',', '.')
         self.custoporitem = float(self.custoporitem)
         self.quantidade = quantidade
         self.p_atual = p_atual
@@ -100,28 +100,102 @@ class Inventario:
         query = 'INSERT INTO inventory (data, item_nome, custo_por_item, numero_de_itens, preço_atual, custo_total, valor_total, porcentagem_retorno_total, retorno_total, item_link) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)'
         self.cursor.execute(query, (data_atual, self.nome, self.custoporitem, self.quantidade, self.p_atual, custo_total, valor_total, porcentagem_retorno_total, retorno_total, self.link))
         self.cursor.connection.commit()
-    
 
-    def obter_preco_atualizado(self, link):
-        hash_link = link[47:]
-        url_destino = 'https://steamcommunity.com/market/priceoverview/?appid=730&currency=7&market_hash_name=' + hash_link
-        url_request = urllib.request.urlopen(url_destino)
-        data = json.loads(url_request.read().decode())
 
-        preco_atualizado = str(data.get('lowest_price'))
-        preco_atualizado = float(preco_atualizado.replace('R$', '').replace(',', '.'))
+    def edit_item(self, item_id, nome, custoporitem, quantidade, p_atual, link):
+        self.item_id = item_id
+        self.nome = nome
+        self.custoporitem = custoporitem
+        self.custoporitem = self.custoporitem.replace(',', '.')
+        self.custoporitem = float(self.custoporitem)
+        self.quantidade = quantidade
+        self.p_atual = p_atual
+        self.link = link
 
-        return preco_atualizado
+        # Atualizar as informações do item no banco de dados
+        self.cursor.execute("UPDATE inventory SET "
+                            "item_nome = ?, "
+                            "custo_por_item = ?, "
+                            "numero_de_itens = ?, "
+                            "preço_atual = ?, "
+                            "item_link = ? "
+                            "WHERE id_item = ?",
+                            (self.nome, self.custoporitem, self.quantidade, self.p_atual, self.link, self.item_id))
+        self.cursor.connection.commit()
 
-    def atualizar_precos(self):
-        self.cursor.execute("SELECT item_link FROM inventory")
-        links = self.cursor.fetchall()
 
-        for link in links:
-            link = link[0]
+    def update_calculations(self, item_id):
+        self.item_id = item_id
 
-            preco_atualizado = self.obter_preco_atualizado(link)
+        self.cursor.execute("SELECT custo_por_item, numero_de_itens, preço_atual FROM inventory WHERE id_item = ?",
+                            (self.item_id,))
+        result = self.cursor.fetchone()
 
-            self.cursor.execute("UPDATE inventory SET preço_atual=? WHERE item_link=?", (preco_atualizado, link))
+        if result:
+            custo_por_item = float(result[0])
+            numero_de_itens = int(result[1])
+            preço_atual = float(result[2])
 
-        self.conexao.commit()
+            total_cost = round(custo_por_item * numero_de_itens, 2)
+            total_value = round(preço_atual * numero_de_itens, 2)
+
+            try:
+                total_return_percent = round(((preço_atual - custo_por_item) / custo_por_item) * 100, 2)
+            except ZeroDivisionError:
+                total_return_percent = 15
+
+            total_return_dollar = round(total_value - total_cost, 2)
+
+            self.cursor.execute("UPDATE inventory SET "
+                                "custo_total = ?, "
+                                "valor_total = ?, "
+                                "porcentagem_retorno_total = ?, "
+                                "retorno_total = ? "
+                                "WHERE id_item = ?",
+                                (total_cost, total_value, total_return_percent, total_return_dollar, self.item_id))
+            self.cursor.connection.commit()
+
+
+    def update_price(self):
+        row = self.cursor.execute("SELECT item_link FROM inventory").fetchall()
+        print(row)  # ================================================================================================================================
+
+        all_links = [item[0] for item in row]  # row é uma lista de tuplas, ele acessa o primeiro elemento da tupla dentro da lista
+        # primeiro elemento da tupla (item[0]) FOR item IN lista de tuplas (row)
+
+        print(all_links)  # ================================================================================================================================
+
+        for url in all_links:
+            print(url)  # ==================================================================================================================================
+            market_hash_name = url[47:]
+            print(f'a partir do 47: {market_hash_name}')  # =================================================================================================
+
+            target_url = "https://steamcommunity.com/market/priceoverview/?appid=730&currency=7&market_hash_name=" + market_hash_name  # + currency=7
+            url_request = urllib.request.urlopen(target_url)
+            data = json.loads(url_request.read().decode())
+            item_name = market_hash_name.replace("%20", " ").replace("%7C", "|").replace("%28", "(").replace("%29", ")").replace(
+                "%26", "&")
+
+            item_price = str(data.get('lowest_price'))
+
+            if item_price:
+                item_price = float(item_price.replace('R$', '').replace(',', '.'))
+            else:
+                item_price = 0.0  # Define um valor padrão para o preço se estiver vazio
+            self.cursor.execute("UPDATE inventory SET preço_atual = ? WHERE item_link = ?", (item_price, url))
+            self.cursor.connection.commit()
+
+
+    def item_list_update(self):
+        self.cursor.execute("SELECT item_nome FROM inventory ORDER BY id_item ASC")
+        items = self.cursor.fetchall()
+        item_list = [item[0] for item in items]
+
+        return item_list
+
+
+    def remove_item(self, item_id):
+        self.item_id = item_id
+
+        self.cursor.execute("DELETE FROM inventory WHERE id_item = ?", (self.item_id,))
+        self.cursor.connection.commit()
--- window.py
+++ window.py
@@ -1,27 +1,23 @@
 from tkinter import *
-from classes import Inventario
 import os
 import sqlite3
-import time
+import datetime
+import json
+import urllib.request
 
+from classes import Inventario
 
 def btn_clicked():
     print("Button Clicked")
 
-window = Tk()
-window.geometry("600x700")
-window.configure(bg="#FFFFFF")
-
 def atualizar_interface():
-    # Atualizar a caixa de texto com os dados do banco de dados
-    texto_multilinhas.delete(0, END)  # Limpar a caixa de texto
+    texto_multilinhas.delete(0, END)
     cursor.execute("SELECT item_nome FROM inventory ORDER BY id_item ASC")
     dados = cursor.fetchall()
     for linha in dados:
         item_nome = linha[0]
         texto_multilinhas.insert(END, item_nome)
 
-    # Atualizar as informações numéricas
     cursor.execute("SELECT SUM(numero_de_itens) FROM inventory")
     resultado = cursor.fetchone()[0]
     resultado = 0 if resultado is None else resultado
@@ -37,9 +33,14 @@ def atualizar_interface():
     r_valor_total_b = 0 if r_valor_total_b is None else round(r_valor_total_b, 2)
     canvas.itemconfig(valor_brut, text=f'{r_valor_total_b} R$')
 
-    cursor.execute("SELECT SUM(valor_total / custo_total) * 0.85 AS valor_descontado FROM inventory")
-    multiplicador = cursor.fetchone()[0]
-    multiplicador = 0 if multiplicador is None else round(multiplicador, 2)
+    cursor.execute("SELECT SUM(valor_total) FROM inventory")
+    result = cursor.fetchone()
+    coluna_valor_total = result[0] if result and result[0] else 0
+    cursor.execute("SELECT SUM(custo_total) FROM inventory")
+    result = cursor.fetchone()
+    coluna_custo_total = result[0] if result and result[0] else 0
+    multiplicador = (coluna_valor_total * 0.85) / coluna_custo_total
+    multiplicador = round(multiplicador, 2) if result else 0
     canvas.itemconfig(mult, text=f'{multiplicador}x')
 
     cursor.execute("SELECT SUM(valor_total) * 0.85 AS valor_descontado FROM inventory")
@@ -51,12 +52,34 @@ def atualizar_interface():
     canvas.itemconfig(lucro_total, text=f'{lucro} R$')
 
 
-bd = 'steamInv.db'
+    lucro = round(r_valor_total_l - r_custo_total, 2)
+    canvas.itemconfig(lucro_total, text=f'{lucro} R$')
 
+def insere_atualiza():
+    inv.get_dados(nome.get(), custo.get(), int(quantidade.get()), preco_atual.get(), market_link.get())
+    atualizar_interface()
+
+def btn_atualizar_inventario():
+    inv.update_price()
+    atualizar_interface()
+
+def get_icon_path():
+    current_dir = os.path.dirname(os.path.abspath(__file__))
+    icon_path = os.path.join(current_dir, "gui", "icone.ico")
+    return icon_path
+
+bd = 'steamInv.db'
 inv = Inventario()
 inv.banco(bd)
 conn = sqlite3.connect(bd)
 cursor = conn.cursor()
+
+window = Tk()
+window.geometry("600x700")
+window.configure(bg="#FFFFFF")
+window.iconbitmap(get_icon_path())
+window.title('Monitor de Inventário - Brook')
+window.resizable(False, False)
 
 canvas = Canvas(
     window,
@@ -72,112 +95,93 @@ canvas.place(x=0, y=0)
 background_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "background.png"))
 background = canvas.create_image(300.0, 350.0, image=background_img)
 
-
-button_ver_inv = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_inv.png"))
-b2 = Button(
-    image=button_ver_inv,
+img_botao_ver_inventario = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_inv.png"))
+botao_ver_inventario = Button(
+    image=img_botao_ver_inventario,
     borderwidth=0,
     highlightthickness=0,
     command=btn_clicked,
     relief="flat"
 )
+botao_ver_inventario.place(x=367, y=578, width=173, height=44)
 
-b2.place(x=367, y=578, width=173, height=44)
-
-def btn_atualizar_inventario():
-    inv.atualizar_precos()
-    atualizar_interface()
-
-button_atualizar = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_atualizar_val.png"))
-b3 = Button(
-    image=button_atualizar,
+img_botao_atualizar = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_atualizar_val.png"))
+botao_atualizar = Button(
+    image=img_botao_atualizar,
     borderwidth=0,
     highlightthickness=0,
     command=btn_atualizar_inventario,
     relief="flat"
 )
+botao_atualizar.place(x=61, y=609, width=173, height=44)
 
-b3.place(x=61, y=609, width=173, height=44)
+img_botao_adicionar = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_adicionar.png"))
+botao_adicionar = Button(
+    image=img_botao_adicionar,
+    borderwidth=0,
+    highlightthickness=0,
+    command=lambda: insere_atualiza(),
+    relief="flat"
+)
+botao_adicionar.place(x=362, y=261, width=173, height=44)
 
-entry0_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox0.png"))
-entry0_bg = canvas.create_image(533.0, 132.5, image=entry0_img)
+img_entrada_nome = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox0.png"))
+canvas.create_image(533.0, 132.5, image=img_entrada_nome)
 nome = StringVar()
-entry0 = Entry(
+entrada_nome = Entry(
     bd=0,
     bg="#324965",
     highlightthickness=0,
     textvariable=nome
 )
+entrada_nome.place(x=480.0, y=124, width=106.0, height=15)
 
-entry0.place(x=480.0, y=124, width=106.0, height=15)
-
-
-entry1_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox1.png"))
-entry1_bg = canvas.create_image(533.0, 151.5, image=entry1_img)
+img_entrada_custo = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox1.png"))
+canvas.create_image(533.0, 151.5, image=img_entrada_custo)
 custo = StringVar()
-entry1 = Entry(
+entrada_custo = Entry(
     bd=0,
     bg="#324965",
     highlightthickness=0,
     textvariable=custo
 )
+entrada_custo.place(x=480.0, y=143, width=106.0, height=15)
 
-entry1.place(x=480.0, y=143, width=106.0, height=15)
-
-entry2_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox2.png"))
-entry2_bg = canvas.create_image(533.0, 170.5, image=entry2_img)
+img_entrada_quantidade = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox2.png"))
+canvas.create_image(533.0, 170.5, image=img_entrada_quantidade)
 quantidade = IntVar()
-entry2 = Entry(
+entrada_quantidade = Entry(
     bd=0,
     bg="#324965",
     highlightthickness=0,
     textvariable=quantidade
 )
+entrada_quantidade.place(x=480.0, y=162, width=106.0, height=15)
 
-entry2.place(x=480.0, y=162, width=106.0, height=15)
-
-entry3_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox3.png"))
-entry3_bg = canvas.create_image(533.0, 189.5, image=entry3_img)
+img_entrada_preco_atual = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox3.png"))
+canvas.create_image(533.0, 189.5, image=img_entrada_preco_atual)
 preco_atual = IntVar()
-entry3 = Entry(
+entrada_preco_atual = Entry(
     bd=0,
     bg="#324965",
     highlightthickness=0,
     textvariable=preco_atual
 )
+entrada_preco_atual.place(x=480.0, y=181, width=106.0, height=15)
 
-entry3.place(x=480.0, y=181, width=106.0, height=15)
-
-entry4_img = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox4.png"))
-entry4_bg = canvas.create_image(487.5, 222.5, image=entry4_img)
+img_entrada_market_link = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "img_textBox4.png"))
+canvas.create_image(487.5, 222.5, image=img_entrada_market_link)
 market_link = StringVar()
-entry4 = Entry(
+entrada_market_link = Entry(
     bd=0,
     bg="#324965",
     highlightthickness=0,
-    textvariable=market_link, 
+    textvariable=market_link,
 )
-
-entry4.place(x=389.0, y=214, width=197.0, height=15)
-
-button_adicionar = PhotoImage(file=os.path.join(os.path.dirname(__file__), "gui", "button_adicionar.png"))
-
-def insere_atualiza():
-    inv.get_dados(nome.get(), custo.get(), quantidade.get(), preco_atual.get(), market_link.get())
-    atualizar_interface()
-
-b1 = Button(
-    image=button_adicionar,
-    borderwidth=0,
-    highlightthickness=0,
-    command = lambda: insere_atualiza(),
-    relief="flat")
-                
-b1.place(x=362, y=261, width=173, height=44)
+entrada_market_link.place(x=389.0, y=214, width=197.0, height=15)
 
 tam_fonte = 10
 
-# numero total de itens
 cursor.execute("SELECT SUM(numero_de_itens) FROM inventory")
 resultado = cursor.fetchone()[0]
 texto_numero = canvas.create_text(505, 415.5, text=resultado, fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
@@ -192,8 +196,6 @@ else:
 
 custo_total_r = canvas.create_text(505, 433.5, text=f'{r_custo_total} R$', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
 
-
-# Valor BRUTO total do inventário
 cursor.execute("SELECT SUM(valor_total) FROM inventory")
 result = cursor.fetchone()
 if result[0] is None:
@@ -202,32 +204,39 @@ else:
     r_valor_total_b = round(result[0], 2)
 valor_brut = canvas.create_text(505, 450.5, text=f'{r_valor_total_b} R$', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
 
-# Multiplicador do valor
-cursor.execute("SELECT SUM(valor_total / custo_total) * 0.85 AS valor_descontado FROM inventory")
-result = cursor.fetchone()
-if result[0] is None:
-    multiplicador = 0
-else:
-    multiplicador = round(result[0], 2)
-mult = canvas.create_text(505, 508.5, text=f'{multiplicador}x', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
-
-# Valor líquido total do inventário
-cursor.execute("SELECT SUM(valor_total) * 0.85 AS valor_descontado FROM inventory")
+cursor.execute("SELECT SUM(valor_total) FROM inventory")
 result = cursor.fetchone()
 if result[0] is None:
     r_valor_total_l = 0
 else:
     r_valor_total_l = round(result[0], 2)
-valor_total_liquido = canvas.create_text(505, 468.5, text=f'{r_valor_total_l} R$', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
+valor_total_liquido = canvas.create_text(505, 468.5, text=f'{r_valor_total_l * 0.85} R$', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
 
-# Lucro
+cursor.execute("SELECT SUM(valor_total) FROM inventory")
+result = cursor.fetchone()
+coluna_valor_total = result[0] if result[0] else 0
+
+cursor.execute("SELECT SUM(custo_total) FROM inventory")
+result = cursor.fetchone()
+coluna_custo_total = result[0] if result[0] else 0
+
+multiplicador = (coluna_valor_total * 0.85) / coluna_custo_total
+print(multiplicador, coluna_valor_total, coluna_custo_total)
+
+if result:
+    multiplicador = round(multiplicador, 2)
+else:
+    multiplicador = 0
+
+mult = canvas.create_text(505, 508.5, text=f'{multiplicador}x', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
+print(multiplicador, coluna_valor_total, coluna_custo_total)
+
 if result[0] is None or r_valor_total_b is None:
     lucro = 0
 else:
-    lucro = round(r_valor_total_l - r_valor_total_b, 2)
+    lucro = round(r_valor_total_l - r_custo_total, 2)
 lucro_total = canvas.create_text(505, 491.5, text=f'{lucro} R$', fill="#ffffff", font=("Alata-Regular", int(tam_fonte)))
 
-# Criação da caixa de texto
 texto_multilinhas = Listbox(
     window,
     bg="#16202d",
@@ -237,21 +246,15 @@ texto_multilinhas = Listbox(
 )
 texto_multilinhas.place(x=18.0, y=95, width=265.0, height=426)
 
-# Recuperar os dados do banco de dados
 cursor.execute("SELECT item_nome, numero_de_itens, custo_por_item, data FROM inventory ORDER BY id_item ASC")
 dados = cursor.fetchall()
 
-# Inserção dos dados na caixa de texto
 for linha in dados:
     item_nome = linha[0]
     numero_de_itens = linha[1]
     custo_por_item = linha[2]
     data = linha[3]
-
-    # Formatar a linha de dados
     linha_formatada = f"{item_nome}"
-
-    # Inserir a linha formatada na caixa de texto
     texto_multilinhas.insert(END, linha_formatada)
 
 def apagar_item_selecionado():    
@@ -275,12 +278,6 @@ b0 = Button(
 
 b0.place(x=61, y=547, width=173, height=44)
 
-def get_icon_path():
-    current_dir = os.path.dirname(os.path.abspath(__file__))
-    icon_path = os.path.join(current_dir, "gui", "icone.ico")
-    return icon_path
+atualizar_interface()  # Atualiza os valores da interface com os valores atuais do banco de dados
 
-window.iconbitmap(get_icon_path())
-window.title('Monitor de Inventário - Brook')
-window.resizable(False, False)
 window.mainloop()
